1b3dcd83d78fd79acb67a06c15427aba9236f9ec
idf <ardek66@tutanota.com>
2025-02-27 20:48:29 +0200

    Use UI containers and more consistent anchoring

--- C7/MainMenu.cs
+++ C7/MainMenu.cs
@@ -97,7 +97,7 @@ public partial class MainMenu : Node2D {
 		newButtonLabel.Theme = theme;
 		newButtonLabel.Text = label;
 
-		TitleCard.GetNode("Menu").AddChild(newButtonLabel);
+		TitleCard.GetNode("MenuPanel/ScrollMenu/Contents").AddChild(newButtonLabel);
 		newButtonLabel.Pressed += action;
 	}
 
